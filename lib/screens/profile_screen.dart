58aa25935cd06cd9b70c84d371386be1e695c1f3
boy <boydoubleb@gmail.com>
2020-07-14 21:12:23 +0700

    [UPDATE] data of user from db to use with profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,17 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import '../models/user.dart';
 import 'auth/auth_screen.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   static const routeName = '/profile';
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  bool _isInit = false;
+  var firstname;
+  var lastname;
+  var address;
+  var postalCode;
+  var phoneNumber;
+
+  fetchUserData() async {
+    final user = Provider.of<User>(context);
+    var info =
+        await Firestore.instance.collection('users').document(user.uid).get();
+    setState(() {
+      firstname = info['firstname'];
+      lastname = info['lastname'];
+      address = info['address'];
+      postalCode = info['postalCode'];
+      phoneNumber = info['phoneNumber'];
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (!_isInit) {
+      fetchUserData();
+    }
+    setState(() {
+      _isInit = true;
+    });
+
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
     if (user != null) {
       return Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
         appBar: AppBar(
           automaticallyImplyLeading: false,
           titleSpacing: 0,
@@ -36,6 +76,121 @@ class ProfileScreen extends StatelessWidget {
                     .copyWith(color: Colors.white),
               ),
             ],
+          ),
+        ),
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(
+                20,
+              ),
+              topRight: Radius.circular(
+                20,
+              ),
+            ),
+          ),
+          child: Container(
+            margin: EdgeInsets.all(
+              20,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
+                  child: Icon(
+                    Icons.account_circle,
+                    size: 132,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Center(
+                  child: firstname != null
+                      ? Text(
+                          '$firstname $lastname',
+                          style: Theme.of(context).textTheme.headline2,
+                        )
+                      : SpinKitCircle(
+                          color: Theme.of(context).primaryColor,
+                        ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Text(
+                  'Your address',
+                  style: Theme.of(context).textTheme.headline3.copyWith(
+                        color: Colors.black,
+                      ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      10,
+                    ),
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  padding: EdgeInsets.all(
+                    20,
+                  ),
+                  child: address != null
+                      ? Text(
+                          '$address $postalCode',
+                          style: Theme.of(context).textTheme.headline3.copyWith(
+                                color: Colors.white,
+                                fontWeight: FontWeight.normal,
+                              ),
+                        )
+                      : SpinKitCircle(
+                          color: Colors.white,
+                        ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  'Your phone number',
+                  style: Theme.of(context).textTheme.headline3.copyWith(
+                        color: Colors.black,
+                      ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      10,
+                    ),
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  padding: EdgeInsets.all(
+                    20,
+                  ),
+                  child: phoneNumber != null
+                      ? Text(
+                          '$phoneNumber',
+                          style: Theme.of(context).textTheme.headline3.copyWith(
+                                color: Colors.white,
+                                fontWeight: FontWeight.normal,
+                              ),
+                        )
+                      : SpinKitCircle(
+                          color: Colors.white,
+                        ),
+                ),
+              ],
+            ),
           ),
         ),
       );
